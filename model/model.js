cb41d2bcecfc351c7f94d65fefa8a86222163eed
wangwei <wangwei@163.com>
2021-01-20 14:44:58 +0800

    完成了 文章分类的curd

---
+++ model/model.js
@@ -0,0 +1,35 @@
+// 封装执行sql语句的promise函数
+var mysql = require('mysql'); 
+
+// 导入数据库的配置参数
+let dbConfig = require("../config/db.json");
+
+
+//连接数据库参数配置
+var connection = mysql.createConnection({
+    // 展开参数
+   ...dbConfig
+});
+
+//连接mysql
+connection.connect(function(err){
+    if(err){
+        throw err;
+    }
+    console.log('connect mysql success');
+});
+
+
+function dbquery(sql){
+    return new Promise((resolve,reject)=>{
+        connection.query(sql,(err,data)=>{
+            if(err){ reject(err); }
+            // select(查) => data是一个数组[{},{},..]  data.length > 说明有数据
+            // insert delete update（增删改） => data是一个对象 data.affectedRows > 0说明成功
+            resolve(data)
+        })
+    })
+}
+
+// 暴露模块的内容
+module.exports = dbquery;
---
+++ router/router.js
@@ -0,0 +1,59 @@
+let express = require('express');
+
+// 得到一个路由器
+let router = express.Router();
+
+
+// 导入相应的控制器
+const CateController = require('../controller/CateController.js');
+
+// 匹配 / 或 /admin
+router.get(/^\/$|^\/admin$/,(req,res)=>{
+    res.render('index.html')
+})
+
+// 文章列表
+router.get('/artindex',(req,res)=>{
+    res.render('article-index.html')
+})
+
+// 渲染后台分类列表页面
+router.get('/catindex',CateController.catindex)
+
+// 渲染出添加分类的页面
+router.get('/catadd',CateController.catadd)
+
+// 渲染出编辑分类的页面
+router.get('/catedit',CateController.catedit)
+
+// 提交分类的数据
+router.post('/postCat',CateController.postCat)
+
+
+router.get('/artadd',(req,res)=>{
+    // res.sendFile( path.join(__dirname,'views/article-add.html') )
+    let data = {name:'西红柿炒蛋'}
+    res.render('article-add.html',data)
+})
+
+// 获取所有分类数据的接口
+router.get('/getCate',CateController.getCate)
+
+
+// 获取所有分类数据的接口
+router.get('/getOneCate',CateController.getOneCate)
+
+// 删除分类的接口
+router.post('/delCat',CateController.delCat)
+
+// 编辑分类的接口
+router.post('/updCate',CateController.updCate)
+
+// 匹配失败的路由
+router.all('*',(req,res)=>{
+    res.json({errcode:10004,message:"请求错误"})
+})
+
+// 暴露出去
+module.exports = router;
+
