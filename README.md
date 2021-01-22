30b3eff5304552a8cd2b1517741c8475f64c3438
wangwei <wangwei@163.com>
2021-01-22 15:04:55 +0800

    导入NProgress效果

--- README.md
+++ README.md
@@ -13,3 +13,27 @@
 3. 执行`npm run serve` 启动项目
 4. 这是在github上面手动修改的，需要执行git pull 进行拉取
 5. again
+
+
+## 加入进度条 NProgress
+
+- 在网页加载完毕之前加载进度条，加载完毕取消进度条即可
+```
+    function startNProgress(){
+        NProgress.set(0.4);// 默认设置40% NProgress.set(0) 等价与 NProgress.start()
+        let interval = setInterval(function(){
+            NProgress.inc(); // 以小量递增
+        },200)
+
+        $(window).on('load',()=>{
+            clearInterval(interval)
+            NProgress.done()
+        })
+    }
+
+    startNProgress();
+```
+
+> 高版本的jquery不支持 ` $(window).on('load')`的写法，改为on绑定即可. [参考](https://www.jianshu.com/p/d1269761bd0a)
+
+
--- public/js/util.js
+++ public/js/util.js
@@ -3,6 +3,25 @@
 
 (function(window,undefined){
 
+    function startNProgress(){
+        NProgress.configure({
+            showSpinner: false
+        });
+        NProgress.set(0.4);// 默认设置40% NProgress.set(0) 等价与 NProgress.start()
+        let interval = setInterval(function(){
+            NProgress.inc(); // 以小量递增
+        },200)
+
+        $(window).on('load',()=>{
+            clearInterval(interval)
+            NProgress.done()
+        })
+    }
+
+    startNProgress();
+
+    
+
     let util = {
         date_format:function(date,format="YYYY-MM-DD HH:mm:ss"){
             return moment(date).format(format)
